a87701656acd373e29fe4a96fa45f0f7287f3ba1
Rayan30 <44604780+Rayan30@users.noreply.github.com>
2018-11-16 18:23:27 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 java.js
 <script type="text/javascript">
-fonction popup () {
+fonction fichierPopUp () {
 var confirme= confirm('Tu veut aller sur ce site pour adultes! Clique sur <ok>') ;
 if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") ; }
 else { alert('Non merci') ; } }
