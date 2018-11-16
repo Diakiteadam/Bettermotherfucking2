f92ad0f2ca2ef7991147c564055865530d29cc14
rayan boudia <rayanboudia93@gmail.com>
2018-11-16 16:33:27 +0100

    Create java.js

---
+++ java.js
@@ -0,0 +1,24 @@
+java.js
+ <script type="text/javascript">
+   fonction popup() {
+var confirme= confirm('Tu veut aller sur ce site pour adultes! Clique sur <ok>') ;
+if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") ; }
+else { alert('Non merci') ; }  }
+
+setTimeout(function() { fichierPop() } , 10000) ;
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
