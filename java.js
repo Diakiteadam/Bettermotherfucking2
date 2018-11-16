0275006306339f84b0fafdff526e03ad7745f8f9
Rayan30 <44604780+Rayan30@users.noreply.github.com>
2018-11-16 17:29:48 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,18 +1,12 @@
 java.js
- <script type="text/javascript">
-   fonction popup() {
-var confirme= confirm('Tu veut aller sur ce site pour adultes! Clique sur <ok>') ;
-if (confirme) { window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") ; }
-else { alert('Non merci') ; }  }
-
-setTimeout(function() { fichierPop() } , 10000) ;
-
-
-
-
-
-   }
-
+setTimeout(
+            function(){
+                document.getElementById('popup').style.display= 'block';
+            }, 10000);
+        
+        document.getElementById('Non').addEventListener('click', function(){
+            document.getElementById('popup').style.display = 'none';
+        });
 
 
 
